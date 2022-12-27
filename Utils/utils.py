a48744be1e5c97a0ce02f5c6d376a623e90f42fe
Varuzhan97 <varuzh2014@gmail.com>
2022-12-27 23:19:33 +0400

    Update

--- Utils/utils.py
+++ Utils/utils.py
@@ -1,6 +1,63 @@
 import os
 import time
 import csv
+import re
+import sox
+import subprocess
+
+SAMPLE_RATE = 16000
+MAX_SECS = 15
+CHANNELS = 1
+
+def check_audio(transcript, wav_filename, items_counter):
+    file_size = -1
+    frames = 0
+    if os.path.exists(wav_filename):
+        file_size = os.path.getsize(wav_filename)
+        frames = int(subprocess.check_output(["soxi", "-s", wav_filename], stderr=subprocess.STDOUT))
+
+    items_counter["all"] += 1
+    items_counter["total_time"] += frames
+
+    if file_size == -1:
+        # Excluding samples that failed upon conversion
+        items_counter["failed"] += 1
+        return False, file_size
+    elif transcript is None:
+        # Excluding samples that failed on label validation
+        items_counter["invalid_label"] += 1
+        return False, file_size
+    elif int(frames / SAMPLE_RATE * 1000 / 10 / 2) < len(str(transcript)):
+        # Excluding samples that are too short to fit the transcript
+        items_counter["too_short"] += 1
+        return False, file_size
+    elif frames / SAMPLE_RATE > MAX_SECS:
+        # Excluding very long samples to keep a reasonable batch-size
+        items_counter["too_long"] += 1
+        return False, file_size
+    else:
+        # This one is good - keep it for the target CSV
+        items_counter["imported_time"] += frames
+
+    return True, file_size
+
+def secs_to_hours(secs):
+    hours, remainder = divmod(secs, 3600)
+    minutes, seconds = divmod(remainder, 60)
+    return '%d:%02d:%02d' % (hours, minutes, seconds)
+
+def get_imported_samples(counter):
+    return counter['all'] - counter['failed'] - counter['too_short'] - counter['too_long'] - counter['invalid_label']
+
+def print_import_report(counter, max_secs):
+    result = ""
+    result += ('Successfully recorded %d samples.' % get_imported_samples(counter) + '\n')
+    result += ('Skipped %d samples that failed upon conversion.' % counter['failed'] + '\n')
+    result += ('Skipped %d samples that failed on transcript validation.' % counter['invalid_label'] + '\n')
+    result += ('Skipped %d samples that were too short to match the transcript.' % counter['too_short'] + '\n')
+    result += ('Skipped %d samples that were longer than %d seconds.' % (counter['too_long'], max_secs) + '\n')
+    result += ('Final amount of imported audio: %s from %s.' % (secs_to_hours(counter['imported_time'] / SAMPLE_RATE), secs_to_hours(counter['total_time'] / SAMPLE_RATE)) + '\n')
+    return result
 
 def write_to_csv(file_name, file_size, transcription, output_folder):
     validated_file_path = os.path.join(output_folder, 'validated.csv')
@@ -17,15 +74,6 @@ def write_to_csv(file_name, file_size, transcription, output_folder):
             csvwriter.writerow([os.path.basename(file_name), str(file_size), transcription])
 
     return validated_file_path
-
-#Remove Audios That Are Shorter Than 0.5 Seconds And Longer Than 20 Seconds
-#Remove Audios That Are Too Short For Transcript
-def check_audio(transcript, file, size):
-    if ((size / 32000) > 0.5 and (size / 32000) < 20 and transcript != "" and size / len(transcript) > 1400):
-        return True
-    else:
-        print("--->|Invalid file.|<---")
-        return False
 
 def generate_alphabet(csv_file_path, output_file_path):
     output_file = os.path.join(output_file_path, 'alphabet.txt')
--- main.py
+++ main.py
@@ -1,54 +1,177 @@
 import os
 import sys
-import argparse
 from datetime import datetime
 from Utils import utils
 from Utils import vad
 
+import tkinter as tk
+
+import threading
+from threading import Event
+from collections import Counter
 
 #For disabling terminal logs
 os.environ['TF_CPP_MIN_LOG_LEVEL'] = '3'
 
-def parse_args():
-    parser = argparse.ArgumentParser(description='Arguments and Their Descriptions.')
-    parser.add_argument('--language_name', default = None,
-                        help='An Argument For The Google Translate\'s API (En, De, Ru, etc.).')
-    parser.add_argument('--text_data', default = None,
-                        help='An Argument For The Absolute Path Of Input Text File.')
-    args = parser.parse_args()
-    if (args.language_name is None) or (args.text_data is None):
-        print('Please Specify --language_name Argument.')
-        sys.exit()
-    return args
+
+class App(tk.Tk):
+    def __init__(self):
+        super().__init__()
+
+        #Configure the root window
+        self.title('Data Annotator')
+        #self.attributes('-fullscreen',True)
+        self.geometry('1500x1000')
+        self.configure(bg='black')
+
+
+        self.columnconfigure(0, weight=1)
+        self.columnconfigure(1, weight=1)
+        self.columnconfigure(2, weight=1)
+
+        self.rowconfigure(0, weight=1)
+        self.rowconfigure(1, weight=1)
+        self.rowconfigure(2, weight=1)
+
+        screen_width = self.winfo_screenwidth()
+        screen_height = self.winfo_screenheight()
+        screen_height = (screen_height/2)
+
+        self.text_label = tk.Text(wrap="word", highlightbackground="black", highlightthickness=5, bg = "black", fg = "white", font=("Ariel", 24), height=10, width=50)
+        self.text_label.tag_configure('center', justify='center')
+
+        self.error_label = tk.Label(highlightbackground="black", highlightthickness=5, bg = "black", fg = "white", font=("Ariel", 12))
+
+        self.text_label.grid(row=0,column=1)
+
+        self.repeat_button = tk.Button(text="Repeat", command = self.repeat)
+        self.invalid_button = tk.Button(text="Invalid", command = self.invalid)
+        self.next_button = tk.Button(text="Next", command = self.next)
+        self.repeat_button.grid(row=1,column=0)
+        self.invalid_button.grid(row=1,column=1)
+        self.next_button.grid(row=1,column=2)
+
+        self.repeat_event = Event()
+        self.invalid_event = Event()
+        self.next_event = Event()
+
+        self.start_proc()
+
+    def repeat(self):
+        self.repeat_event.set()
+
+    def invalid(self):
+        self.invalid_event.set()
+
+    def next(self):
+        self.next_event.set()
+
+
+    def check_speech(self, event):
+        while True:
+            if event.is_set():
+                self.text_label["highlightbackground"] = "red"
+            else:
+                self.text_label["highlightbackground"] = "black"
+
+
+    def start_read(self, event):
+        main_dir = os.getcwd()
+
+        #Start the course
+        #Make data collection path for language
+        output_folder = os.path.join(main_dir, "Collection", datetime.now().strftime("%Y-%m-%d_%H-%M-%S"))
+        text_file = os.path.join(main_dir, "Data", "input.txt")
+        os.makedirs(output_folder, exist_ok=True)
+
+        self.vad_audio = vad.listen_audio(event, output_folder)
+
+        input_data = open(text_file, "r")
+        file_data = input_data.readlines()
+        input_data.close()
+
+        items_counter = Counter({'all': 0, 'failed': 0, 'invalid_label': 0, 'too_short': 0, 'too_long': 0, 'imported_time': 0, 'total_time': 0})
+
+        j = 0
+
+        #for line in file_data:
+        while j < len(file_data):
+            #Remove spaces from both side(begining and end) of string and return
+            line = file_data[j].strip()
+            #Load user speech
+            file_name = str()
+            self.text_label.delete("1.0", tk.END)
+            self.text_label.insert(tk.END, line, 'center')
+
+            self.next_event.clear()
+            self.next_button["state"] = "disabled"
+
+            self.invalid_event.clear()
+            self.invalid_button["state"] = "disabled"
+
+            self.repeat_event.clear()
+            self.repeat_button["state"] = "disabled"
+
+            file_name = next(self.vad_audio)
+
+            self.next_event.clear()
+            self.next_button["state"] = "normal"
+
+            self.invalid_event.clear()
+            self.invalid_button["state"] = "normal"
+
+            self.repeat_event.clear()
+            self.repeat_button["state"] = "normal"
+
+            #Check for audio length and update csv
+            while True:
+                if self.next_event.is_set():
+                    self.next_event.clear()
+
+                    is_valid, file_size = utils.check_audio(line, file_name, items_counter)
+                    if is_valid:
+                        #Update collected data in csv
+                        utils.write_to_csv(file_name, file_size, line, output_folder)
+                    else:
+                        #print("--->|File is not valid.|<---")
+                        os.remove(file_name)
+
+                    j+=1
+                    break
+
+                if self.invalid_event.is_set():
+                    self.invalid_event.clear()
+                    #print("--->|File is not valid.|<---")
+                    os.remove(file_name)
+                    j+=1
+                    break
+
+                if self.repeat_event.is_set():
+                    self.repeat_event.clear()
+                    os.remove(file_name)
+                    break
+
+        csv_file_path = os.path.join(output_folder, "validated.csv")
+        utils.generate_alphabet(csv_file_path, output_folder)
+
+        self.text_label.delete("1.0", tk.END)
+        self.text_label.insert(tk.END, utils.print_import_report(items_counter, 15), 'center')
+
+    def start_proc(self):
+
+        self.anim_event = Event()
+        self.language_event = Event()
+
+        self.anim_proc = threading.Thread(target=self.start_read, args = (self.anim_event, ))
+        #Daemon threads are those threads which are killed when the main program exits
+        self.anim_proc.daemon = True
+        self.anim_proc.start()
+
+        self.vad_proc = threading.Thread(target=self.check_speech, args = (self.anim_event, ))
+        #Daemon threads are those threads which are killed when the main program exits
+        self.vad_proc.daemon = True
+        self.vad_proc.start()
 
 if __name__ == "__main__":
-    params = parse_args()
-    main_dir = os.getcwd()
-
-    #Start the course
-    #Make data collection path for language
-    collection_folder = os.path.join(main_dir, "Collection")
-    output_folder = os.path.join(collection_folder, params.language_name + "-" + datetime.now().strftime("%Y-%m-%d_%H-%M-%S"))
-    os.makedirs(output_folder, exist_ok=True)
-    # Start audio with VAD
-    vad_audio = vad.VADAudio(aggressiveness = 3, input_rate=16000)
-    input_data = open(params.text_data, "r")
-    file_data = input_data.readlines()
-
-    for line in file_data:
-        #Remove spaces from both side(begining and end) of string and return
-        line = line.strip()
-        #Load user speech
-        file_name = str()
-        file_size = str()
-        file_name, file_size = vad.listen_audio(vad_audio, line, output_folder)
-        #Check for audio length and update csv
-        if utils.check_audio(line, file_name, file_size):
-            #Update collected data in csv
-            utils.write_to_csv(file_name, file_size, line, output_folder)
-        else:
-            print("--->|File is not valid.|<---")
-            os.remove(file_name)
-
-    csv_file_path = os.path.join(output_folder, "validated.csv")
-    utils.generate_alphabet(csv_file_path, output_folder)
+    app = App()
+    app.mainloop()
